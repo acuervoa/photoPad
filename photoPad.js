8ddec6c6aa26f4be491dccf669bf8443dcd941a0
acuervoa <kuerbo@gmail.com>
2016-09-16 22:04:35 +0200

    Load and save image

--- photoPad.js
+++ photoPad.js
@@ -1,34 +1,88 @@
 "use strict";
 
-function MyApp()
+function PhotoPadApp()
 {
-	var version = " v1.0";
+	var version = " v5.2",
+		canvas = $("#main>canvas")[0],
+		context = canvas.getContext("2d"),
+		$img = $("<img>"),
+		toolbar = new Toolbar($("#toolbar"));
 
 	function setStatus(message) {
 
 		$("#app>footer").text(message);
 	}
 
+
 	this.start = function() {
 		$("#app>header").append(version);
-		setStatus("ready");
+		//setStatus("ready");
+
+		if(window.File && window.FileReader)
+		{
+			$("#load-menu input[type=file]").change(function(e) {
+				onLoadFile($(this));
+			});
+		}
+		else
+		{
+			loadImage("images/default.jpg");
+		}
+
+		toolbar.toolbarButtonClicked = toolbarButtonClicked;
+	}
+
+	function toolbarButtonClicked(action)
+	{	
+
+		console.log(action);
+		switch(action)
+		{
+			case "save":
+				var url = canvas.toDataURL();
+				window.open(url, "PhotoPadImage");
+				break;
+		}
+	}
+
+	function onLoadFile($input)
+	{
+		var file = $input[0].files[0];
+		if(file.type.match("image.*"))
+		{
+			var reader = new FileReader();
+			reader.onload = function() {loadImage(reader.result); };
+			reader.readAsDataURL(file);
+		}
+		else
+		{
+			alert("Not a valid image type: " + file.type);
+			setStatus("Error loading image!");
+		}
+	}
+
+	function loadImage(url)
+	{
+		setStatus("Loading image");
+		$img.attr("src", url);
+		$img[0].onload = function()
+		{
+			// Here "this" is the image
+			canvas.width = this.width;
+			canvas.height = this.height;
+			context.drawImage(this, 0, 0);
+			setStatus("Choose an effect");
+		}
+		$img[0].onerror = function()
+		{
+			setStatus("Error loading image!");
+		}
 	}
 }
 
-/*var myApp = {
-	version: "v1.0",
-	setStatus: function(message)
-	{
-		$("#app>footer").text(message);
-	},
-	start: function()
-	{
-		$("#app>header").append(this.version);
-		this.setStatus("ready");
-	}
-}*/
+
 
 $(function() {
-	window.app = new MyApp();
+	window.app = new PhotoPadApp();
 	window.app.start();
 });
